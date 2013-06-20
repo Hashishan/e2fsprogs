83a6a4afdf778bdc3f589429b0fb3d4876e320a6
Tehmasp Chaudhri <tehmasp@gmail.com>
2013-06-19 22:42:38 -0600

    converted from double to single quotes, fixed maintainer name/email address/license

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
-name             "e4fsprogs"
-maintainer       "Rally Software"
-maintainer_email "eklein@rallydev.com"
-license          "All rights reserved"
-description      "Installs/Configures e4fsprogs"
+name             'e4fsprogs'
+maintainer       'Rally Software Development Corp'
+maintainer_email 'rallysoftware-cookbooks@rallydev.com'
+license          'MIT'
+description      'Installs/Configures e4fsprogs'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          '0.1.0'
